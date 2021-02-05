6661a505ed7c440117979f6a7ce496e33ef95275
William Bugenis <wbugenis@gmail.com>
2021-02-05 17:39:14 -0500

    commit tree 954d8dafff53d7f22f540c97d0484f81f8bdc0a6

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,40 @@
+class SchoolClassesController < ApplicationController
+
+    def index
+        @school_classes = SchoolClass.all
+    end
+
+    def show
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def new
+        @school_class = SchoolClass.new
+    end
+
+    def create
+        @school_class = SchoolClass.create(school_class_params)
+        redirect_to school_class_path(@school_class)
+    end
+
+    def edit
+        @school_class = SchoolClass.find(params[:id])
+    end
+
+    def update
+        @school_class = SchoolClass.find(params[:id])
+        @school_class.update(school_class_params)
+        redirect_to school_class_path(@school_class)
+    end
+
+    def destroy
+        SchoolClass.find(params[:id]).destroy
+        redirect_to school_classes_path
+    end
+
+    private
+
+    def school_class_params
+        params.require(:school_class).permit(:title, :room_number)
+    end
+end
